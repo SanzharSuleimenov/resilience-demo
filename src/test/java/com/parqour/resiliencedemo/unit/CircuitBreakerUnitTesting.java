dc7a107f4d14442fb1a098580996f8fb55cb6a62
Sanzhar Suleimenov <s.suleimenov@parqour.com>
2024-01-09 15:46:01 +0600

    Integration test of CB

--- src/test/java/com/parqour/resiliencedemo/unit/CircuitBreakerUnitTesting.java
+++ src/test/java/com/parqour/resiliencedemo/unit/CircuitBreakerUnitTesting.java
@@ -10,14 +10,15 @@ import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import com.parqour.resiliencedemo.ResilienceDemoApplication;
+import com.parqour.resiliencedemo.adapter.out.persistence.ParkingRoute;
 import com.parqour.resiliencedemo.application.port.out.GetParkingRoutePort;
 import com.parqour.resiliencedemo.application.port.out.TopUpBalancePort;
 import com.parqour.resiliencedemo.application.port.service.UpstreamService;
-import com.parqour.resiliencedemo.adapter.out.persistence.ParkingRoute;
 import com.parqour.resiliencedemo.service.PaymentService;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import java.util.Optional;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
@@ -90,7 +91,7 @@ public class CircuitBreakerUnitTesting {
   @Test
   void requestPayWhenCircuitBreakerIsOpen() {
     circuitBreakerRegistry.circuitBreaker(UpstreamService.CB_EXTERNAL_SERVICE)
-        .transitionToForcedOpenState();
+        .transitionToOpenState();
 
     ParkingRoute tempParkingRoute = new ParkingRoute(1L, "localhost", 8080, "my local laptop", "1");
 
@@ -104,6 +105,49 @@ public class CircuitBreakerUnitTesting {
     } catch (RuntimeException e) {
       assertEquals(CallNotPermittedException.class, e.getClass());
       verifyNoInteractions(topUpBalancePort);
+    }
+  }
+
+  @Test
+  void requestMinNumberOfFailedRequestAndExpectCircuitBreakerTransitionToOpenState() {
+    circuitBreakerRegistry.circuitBreaker(UpstreamService.CB_EXTERNAL_SERVICE)
+        .transitionToClosedState();
+
+    ParkingRoute tempParkingRoute = new ParkingRoute(1L, "localhost", 8080, "my local laptop", "1");
+    ParkingRoute differentRoute = new ParkingRoute(2L, "localhost", 8080, "my local laptop", "2");
+    ParkingRoute thirdRoute = new ParkingRoute(3L, "localhost", 8080, "my local laptop", "3");
+
+    when(getParkingRoutePort.findByUid(eq("1")))
+        .thenReturn(Optional.of(tempParkingRoute));
+    when(getParkingRoutePort.findByUid(eq("2")))
+        .thenReturn(Optional.of(differentRoute));
+
+    // somehow Circuit Breaker tracks these two invocations and applies them to buffered calls count
+    doThrow(new RuntimeException()).when(topUpBalancePort).topUpBalance(tempParkingRoute);
+    doReturn("topped up").when(topUpBalancePort).topUpBalance(differentRoute);
+    doThrow(new RuntimeException("Bad request")).when(topUpBalancePort)
+        .topUpBalance(thirdRoute);
+
+    try {
+      int minNumberOfCalls = circuitBreakerRegistry
+          .circuitBreaker(UpstreamService.CB_EXTERNAL_SERVICE)
+          .getCircuitBreakerConfig()
+          .getMinimumNumberOfCalls() - 3;
+
+      IntStream.rangeClosed(1, minNumberOfCalls)
+          .forEach((i) -> {
+            try {
+              paymentService.pay("123TEST02", "1");
+            } catch (RuntimeException e) {
+              assertEquals(RuntimeException.class, e.getClass());
+            }
+          });
+
+      paymentService.pay("123TEST03", "2");
+
+      fail("Test scenario expects failure at this point");
+    } catch (RuntimeException e) {
+      assertEquals(CallNotPermittedException.class, e.getClass());
     }
   }
 }
